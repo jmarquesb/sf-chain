322ee28a4e9679657f46ecac277ee32401e1ea68
jmarquesb <jmarquesb@gmail.com>
2018-04-26 14:30:04 +0200

    Initial commit

---
+++ dev-test.js
@@ -0,0 +1,19 @@
+/*const Block = require('./block');
+
+const block = new Block('foo', 'bar', 'zoo', 'baz');
+console.log(block.toString());
+console.log(Block.genesis().toString());
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
+
+const blockChain = require('./blockchain');
+
+const bc = new blockChain();
+
+for (let i=0; i< 10; i++){
+    console.log(bc.addBlock(`foo ${i}`).toString());
+}*/
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
